289b1ed74ba5106c06371468cfd4531329185509
Kiramuun <deimonlight@live.fr>
2023-10-16 16:57:21 +0200

    Update Shéma.cs

--- Shéma.cs
+++ Shéma.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System.Diagnostics.Metrics;
+using System.Security.Cryptography;
 
 string LabyEnsorceler = "╔═════════╦════╦════════════════════════╥════════╗" +//0 a 49
                         "║░░░░░░░░░╢░░░░║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║" +//50 a 99
@@ -24,34 +25,79 @@ const int LongueurX = 899,
           Entrée = 400,
           Sortie = 849;
 
-ConsoleKeyInfo c = Console.ReadKey(true);
-ConsoleKey Right = c.Key,
-           Left = c.Key,
-           Up = c.Key,
-           Down = c.Key,
-           Enter = c.Key;
-
+int PositionJoueur = Entrée;
 int Murs = 0,
-    Position = 0;
+        Position = 0;
 
-while (LabyEnsorceler.Length == Position)
+Main();
+
+void Intro()
 {
-    if ((LabyEnsorceler[Position] == '╔' )|(LabyEnsorceler[Position] == '╗') | (LabyEnsorceler[Position] == '╝')|(LabyEnsorceler[Position] == '╚') | (LabyEnsorceler[Position] == '║') | (LabyEnsorceler[Position] == '═')| (LabyEnsorceler[Position] == '█') | (LabyEnsorceler[Position] == '╣') | (LabyEnsorceler[Position] == '╦') | (LabyEnsorceler[Position] == '╩') | (LabyEnsorceler[Position] == '╠'))
-    {
-        Murs++;
-        Position++;
-    }
-    else { Position++; }
+    Console.WriteLine("Bienvenue");
+    Console.WriteLine("Appuie sur une touche pour embarquer...");
+    Console.ReadKey();
+    Console.WriteLine();
 }
 
-string FormeJoueur = "■";
-int PositionJoueur = Entrée;
-
-if(Enter == ConsoleKey.Enter)
+void Main()
 {
-    while(true)
+    Intro();
+
+
+    while (LabyEnsorceler.Length == Position)
     {
-        
+        if ((LabyEnsorceler[Position] == '╔') | (LabyEnsorceler[Position] == '╗') | (LabyEnsorceler[Position] == '╝') | (LabyEnsorceler[Position] == '╚') | (LabyEnsorceler[Position] == '║') | (LabyEnsorceler[Position] == '═') | (LabyEnsorceler[Position] == '█') | (LabyEnsorceler[Position] == '╣') | (LabyEnsorceler[Position] == '╦') | (LabyEnsorceler[Position] == '╩') | (LabyEnsorceler[Position] == '╠'))
+        {
+            Murs++;
+            Position++;
+        }
+        else { Position++; }
+    }
+
+    string FormeJoueur = "■";
+    
+
+    while(PositionJoueur != Sortie)
+    {
+        Input();
+        DrawLaby();
+    }
+
+}
+
+void DrawLaby()
+{
+    Console.WriteLine("╔═════════╦════╦════════════════════════╥════════╗\n" +//0 a 49
+                            "║░░░░░░░░░╢░░░░║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n" +//50 a 99
+                            "║░░░░░░░░░╠════╝░░░░╔════╗░░░══════════╗░░░░░║░░░║\n" +//100 a 149
+                            "║░░░░S░░░░║░░░░░░░░░║░░░░║░░░░░░░░░░░░░║░░░░░║░░░║\n" +//150 a 199
+                            "║░░░░░░░░░║░░░░░░░░░║░░░░╚═════════════╣░░░░░║░░░║\n" +//200 a 249
+                            "║░░░░░░░░░║░░░░╔════╣░░░░░░░░░░░░░░░░░░║░░░░░║░░░║\n" +//250 a 299
+                            "╠═════════╝░░░░║░░░░╚═════════════╗░░░░║░░░░░║░░░║\n" +//300 a 349
+                            "║░░░░░░░░░░░░░░┤░░░░░░░░░░░░░░░░░░║░░░░░░░░░░║░░░║\n" +//350 a 399
+                            "▒░░░╔════════╦═╩═════░░░░════╦════╣░░░╔══════╣░░░║\n" +//400 a 449
+                            "║░░░║░░░░░░░░║░░░░░░░░░░░░░░░║░░░░╠═══╝░░░░░░║░░░║\n" +//450 a 499
+                            "║░░░║░░╔══╗░░║░░░░░╔══╦═╥═╦══╣░░░░║░░░░░░░░░░║░░░║\n" +//500 a 549
+                            "║░░░║░░║██║░░║░░░░░║██║░░░║██║░░░░║░░░░╔════╧╣░░░║\n" +//550 a 599
+                            "║░░░╚┬═╩══╩╨═╣░░░░░║██║░░░║██║░░░░║░░░░║████S║░░░║\n" +//600 a 649
+                            "║░░░░░░░░░░░░║░░░░░║██║░░░║██║░░░░║░░░░╚═════╝░░░║\n" +//650 a 699
+                            "║░░░░╔═══╗░░░║░░░░░║██║░░░║██║░░░░░░░░░░░░░░░░░░░║\n" +//700 a 749
+                            "║░░░░╚═══╝░░░║░░░░░╚══╩═══╩══╩══╦════════════════╣\n" +//750 a 799
+                            "║░░░░░░░░░░░░║░░░░░░░░░░░░░░░░░░┤░░░░░░░░░░░░░░░░▒\n" +//800 a 849
+                            "╚════════════╩══════════════════╩════════════════╝\n");
+    Console.WriteLine("Le Joueur est a la position " + PositionJoueur);
+}
+void Input()
+{
+    ConsoleKeyInfo c = Console.ReadKey(true);
+    ConsoleKey Right = c.Key,
+               Left = c.Key,
+               Up = c.Key,
+               Down = c.Key,
+               Enter = c.Key;
+
+    if (Enter == ConsoleKey.Enter)
+    {
         if (Right == ConsoleKey.RightArrow)
         {
             if (PositionJoueur == Murs)
@@ -86,22 +132,3 @@ if(Enter == ConsoleKey.Enter)
         }
     }
 }
-
-Console.WriteLine("╔═════════╦════╦════════════════════════╥════════╗\n" +//0 a 49
-                        "║░░░░░░░░░╢░░░░║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n" +//50 a 99
-                        "║░░░░░░░░░╠════╝░░░░╔════╗░░░══════════╗░░░░░║░░░║\n" +//100 a 149
-                        "║░░░░S░░░░║░░░░░░░░░║░░░░║░░░░░░░░░░░░░║░░░░░║░░░║\n" +//150 a 199
-                        "║░░░░░░░░░║░░░░░░░░░║░░░░╚═════════════╣░░░░░║░░░║\n" +//200 a 249
-                        "║░░░░░░░░░║░░░░╔════╣░░░░░░░░░░░░░░░░░░║░░░░░║░░░║\n" +//250 a 299
-                        "╠═════════╝░░░░║░░░░╚═════════════╗░░░░║░░░░░║░░░║\n" +//300 a 349
-                        "║░░░░░░░░░░░░░░┤░░░░░░░░░░░░░░░░░░║░░░░░░░░░░║░░░║\n" +//350 a 399
-                        "▒░░░╔════════╦═╩═════░░░░════╦════╣░░░╔══════╣░░░║\n" +//400 a 449
-                        "║░░░║░░░░░░░░║░░░░░░░░░░░░░░░║░░░░╠═══╝░░░░░░║░░░║\n" +//450 a 499
-                        "║░░░║░░╔══╗░░║░░░░░╔══╦═╥═╦══╣░░░░║░░░░░░░░░░║░░░║\n" +//500 a 549
-                        "║░░░║░░║██║░░║░░░░░║██║░░░║██║░░░░║░░░░╔════╧╣░░░║\n" +//550 a 599
-                        "║░░░╚┬═╩══╩╨═╣░░░░░║██║░░░║██║░░░░║░░░░║████S║░░░║\n" +//600 a 649
-                        "║░░░░░░░░░░░░║░░░░░║██║░░░║██║░░░░║░░░░╚═════╝░░░║\n" +//650 a 699
-                        "║░░░░╔═══╗░░░║░░░░░║██║░░░║██║░░░░░░░░░░░░░░░░░░░║\n" +//700 a 749
-                        "║░░░░╚═══╝░░░║░░░░░╚══╩═══╩══╩══╦════════════════╣\n" +//750 a 799
-                        "║░░░░░░░░░░░░║░░░░░░░░░░░░░░░░░░┤░░░░░░░░░░░░░░░░▒\n" +//800 a 849
-                        "╚════════════╩══════════════════╩════════════════╝\n");
